657598f698c8993dbe21f015a3ea648bd487ffd4
enochsori <enoch.ha915@gmail.com>
2023-01-25 22:44:33 -0500

    chapter 2

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,35 @@
 /**
  * Let's make a calculator 🧮
- */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+ */ type Command = "add" | "substract" | "multiply" | " divide" | "remaider";
+const calculate = (unit: Command, num1: number, num2: number) => {
+  switch (unit) {
+    case "add": {
+      return num1 + num2;
+      break;
+    }
+    case "substract": {
+      return num1 - num2;
+      break;
+    }
+    case "multiply": {
+      return num1 * num2;
+      break;
+    }
+    case "divide": {
+      return num1 / num2;
+      break;
+    }
+    case "remainder": {
+      return num1 % num2;
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
